1b4beef723976d231e33664e535e71374ae675f3
AlessandroPro03 <23121018@unamad.edu.pe>
2025-12-19 02:23:47 -0500

    acceso a cocina

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Registro from './pages/Acceso/registro.jsx'
 import DashboardAdmin from './pages/admin/dasboard-admin.jsx'
 import Mozo from './pages/mozo/mozo.jsx'
 import AdminMesas from './pages/admin/admin-mesas.jsx'
-import DashboardCajero from './pages/cajero/dasboardcajero.jsx'
+import DashboardCajero from './pages/cajero/DashboardCajero.jsx'
 import Cocina from './pages/Cocina/cocina.jsx'
 function App() {
   return (
